51b67bbd815a6f9b7e0fb05b5b3dc68b01d90af2
Rodney Lab <codesign@rodneylab.com>
2024-01-03 19:40:50 +0000

    fix: 💫 add player

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,80 @@
 #![warn(clippy::all, clippy::pedantic)]
+use macroquad::color::colors::{DARKBLUE, SKYBLUE, YELLOW};
 use macroquad::prelude::*;
 
-#[macroquad::main("BasicShapes")]
+struct Flipper {
+    x_displacement: f32,
+    y_displacement: f32,
+    acceleration: f32,
+    velocity: f32,
+}
+
+impl Default for Flipper {
+    fn default() -> Self {
+        Flipper {
+            x_displacement: 20.0,
+            y_displacement: 0.5 * screen_height() - 10.0,
+            acceleration: -0.5,
+            velocity: 0.0,
+        }
+    }
+}
+
+impl Flipper {
+    fn draw(&self) {
+        draw_rectangle(
+            self.x_displacement,
+            self.y_displacement,
+            20.0,
+            20.0,
+            SKYBLUE,
+        );
+    }
+
+    fn flap(&mut self) {
+        if self.velocity > -3.0 {
+            self.velocity += self.acceleration;
+        }
+    }
+
+    fn update(&mut self) {
+        // gravity
+        if self.velocity < 0.5 {
+            self.velocity += 0.1;
+        }
+
+        if self.y_displacement < 0.0 {
+            self.y_displacement = 0.0;
+        } else if self.y_displacement > screen_height() - 20.0 {
+            self.y_displacement = screen_height() - 20.0;
+        } else {
+            self.y_displacement += self.velocity;
+        }
+    }
+}
+
+#[derive(Default)]
+struct GameState {
+    flipper: Flipper,
+}
+
+#[macroquad::main("Flipper")]
 async fn main() {
+    let mut game_state = GameState::default();
+
     loop {
-        clear_background(RED);
+        let GameState { ref mut flipper } = game_state;
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+        if is_key_down(KeyCode::Space) {
+            flipper.flap();
+        }
 
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+        clear_background(DARKBLUE);
+
+        flipper.update();
+        flipper.draw();
+
+        draw_text("Press SPACE to soar", 20.0, 20.0, 30.0, YELLOW);
 
         next_frame().await;
     }
